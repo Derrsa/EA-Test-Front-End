413fb8fc85940e16baed6452474f2880b119aeb8
Derrsa <igrimasok@gmail.com>
2024-05-11 13:43:51 +0300

    finish work

--- script/script.js
+++ script/script.js
@@ -4,44 +4,45 @@ let day_block = document.querySelector('.timer__day .day__number')
 let hour_block = document.querySelector('.timer__hour .day__number')
 let minute_block = document.querySelector('.timer__minute .day__number')
 let second_block = document.querySelector('.timer__sec .day__number')
-
-
-
 window.onload = function() {
     const timer = setInterval(() => {
         const now = new Date().getTime();
         const remaining = endTime - now;
 
+        //Создаем переменные для получения цифр разницы между текущей и целевой датой
         const days = Math.floor(remaining / (24 * 60 * 60 * 1000));
         const hours = Math.floor((remaining % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
         const mins = Math.floor((remaining % (60 * 60 * 1000)) / (60 * 1000));
         const secs = Math.floor((remaining % (60 * 1000)) / 1000);
 
+        // Добавляем 0 к цифрам меньше 10
         day_block.textContent = days < 10 ? `0${days}` : String(days)
         hour_block.textContent = hours < 10 ? `0${hours}` : String(hours)
         minute_block.textContent = mins < 10 ? `0${mins}` : String(mins)
         second_block.textContent = secs < 10 ? `0${secs}` : String(secs)
-
-
-
-
+        // Окончание таймера
         if (remaining < 0) {
             clearInterval(timer);
             document.getElementById("demo").textContent = "Время истекло";
         }
     }, 1000);
 }
+
+
+
 // ============================VALIDATION=====================================
+
+//Валидация инпута с помощью регулярки
 function validateEmail(email) {
-    let re = /^(([^&lt;&gt;()\[\]\\.,;:\s@"]+(\.[^&lt;&gt;()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
 
 
 const form = document.querySelector('.footer__form')
 let email = document.querySelector('.form__input')
 let formBtn = document.querySelector('.form__button')
-
+// Фунцкия которая отправляет на сервер информация из инпута
 function postData(url, data) {
     return fetch(url, {
         method: 'POST',
@@ -61,6 +62,9 @@ function postData(url, data) {
             throw error;
         });
 }
+
+
+// ====================================MODAL====================================
 const modal = document.getElementById('modal-success')
 const modalFail =document.getElementById('modal-reject')
 document.querySelector('.modal__close').addEventListener('click', ()=>{
@@ -79,7 +83,7 @@ modal.addEventListener('click', el=>{
 
 formBtn.addEventListener('click', (el) => {
     el.preventDefault()
-    if(validateEmail(email.value)){
+    if(!validateEmail(email.value)){
         email.classList.add('error')
     } else {
         email.classList.remove('error')
@@ -133,6 +137,16 @@ changeTimerBlockName()
 window.addEventListener('resize', () =>{
     changeTimerBlockName()
 })
+// Плавное появление элементов на странице
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.animated-element');
 
+    elements.forEach((element, index) => {
+        // Добавляем класс с задержкой для каждого элемента
+        setTimeout(() => {
+            element.classList.add('animate');
+        }, index * 200); // Увеличивайте значение умножения для большей задержки между элементами
+    });
+});
 
 
